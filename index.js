86603f741ab870f98bcc5860c1aefe0650cdde10
Rohit Patil <32801968+RohitPatilRRP@users.noreply.github.com>
2019-10-04 13:19:08 +0530

    MongoDb to NodeJs connection
    Basic integration of MongoDb with NodeJs

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const MongoClient = require('mongodb').MongoClient;
+let Url = 'mongodb://localhost:27017/';
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("testDb");
-  var myobj = { name: "mrRob", address: "RobHouse" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("document inserted");
-    db.close();
-  });
+MongoClient.connect(Url, function(err, db) {
+  if (err) 
+  {
+    throw err; 
+  }
+  else {
+    //Add your logic here
+  }
 });
